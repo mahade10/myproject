24b702a1b0a3ea2d9bb4cc9b14dd9410b26a2af6
mahade hasan abir <mahadeabirabir@gmail.com>
2018-04-18 22:50:25 +0600

    make show function

--- to_do_list.cpp
+++ to_do_list.cpp
@@ -16,6 +16,30 @@ struct list data[100];
 
 
 
+bool Add_in_file(string name,string description, string Date)
+{
+
+   if (listSize < 101)
+   {
+      list y = { name,description, Date };
+      data[listSize] = y;
+      listSize++;
+      return true;
+   }
+   else
+   {
+      return false;
+   }
+}
+
+
+void Show_file()
+{
+
+}
+
+
+
 int main()
 {
    int choice = 0;
