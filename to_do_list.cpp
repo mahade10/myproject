b354d129b34f4a9c93455197cd829b052bff5a82
mahade hasan abir <mahadeabirabir@gmail.com>
2018-04-18 22:42:28 +0600

    prepare add function for  data store

--- to_do_list.cpp
+++ to_do_list.cpp
@@ -21,6 +21,9 @@ bool Add_in_file(string name,string description, string Date)
 
    if (listSize < 101)
    {
+      list y = { name,description, Date };
+      data[listSize] = y;
+      listSize++;
       return true;
    }
    else
